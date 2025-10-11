228999817d07b5cd635a2e9ee798dd9aabac6de8
Abhinav Anil <abhinavanil800@gmail.com>
2025-10-11 21:32:40 +0530

    Added_get_User-Data_API

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import cookieParser from "cookie-parser"
 import connectDb from "./config/mongodb.js"
 import authRouter from "./routes/authRoutes.js"
+import userRouter from "./routes/userRoutes.js"
 
 
 const app = express()
@@ -19,6 +20,8 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/api/auth',authRouter)
+app.use('/api/user',userRouter)
+
 
 app.listen(PORT,()=>{
     console.log("Server running on ", PORT)
