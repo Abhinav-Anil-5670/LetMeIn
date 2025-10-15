abaa16033481db5bea4bb85beeadbbf4941cf9b1
Abhinav Anil <abhinavanil800@gmail.com>
2025-10-15 22:45:03 +0530

    Email_Verification_API_Link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,6 +16,24 @@ const Navbar = () => {
     getUserData,
   } = useContext(AppContext);
 
+  const sendVerification = async ()=>{
+    try{
+      axios.defaults.withCredentials = true
+      const {data} = await axios.post(`${backendURL}/api/auth/send-verify-otp`)
+
+      if (data.success){
+        navigate('/email-verify')
+        toast.success('Email Sent Successfully')
+      }
+      else{
+        toast.error(data.message)
+      }
+    }
+    catch(err){
+      toast.error(err.message)
+    }
+  }
+
   const logout = async ()=>{
     try{
       axios.defaults.withCredentials = true
@@ -40,7 +58,7 @@ const Navbar = () => {
           {userData.name[0].toUpperCase()}
           <div className="absolute hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-10">
 
-            <ul className="list-none m-0 p-2 bg-gray-100 text-sm">
+            <ul onClick={sendVerification} className="list-none m-0 p-2 bg-gray-100 text-sm">
               {!userData.isAccountVerified && <li className="py-1 px-2 hover:bg-gray-200 cursor-pointer">Verify Email</li>}
               
               <li onClick={logout} className="py-1 px-2 hover:bg-gray-200 cursor-pointer pr-10">Logout</li>
